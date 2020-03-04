ae9bd12f786a79ca247afe5bedf523b4c7f29573
Do Khac Phong <phong@conceptual.studio>
2020-03-05 00:28:31 +0700

    nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,6 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    // '~/plugins/vue-notifications'
     { src: '~/plugins/initLibraries.js', mode: 'client' }
   ],
   /*
